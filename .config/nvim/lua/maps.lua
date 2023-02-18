415a24c937acd1166750291604aa3995dd157b57
Carlos Sluzala <wdcarloss@gmail.com>
2023-02-18 17:30:28 -0300

    base config

---
+++ .config/nvim/lua/maps.lua
@@ -0,0 +1,38 @@
+local keymap = vim.keymap
+
+-- Do not yan with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n',  '+', '<C-a>')
+keymap.set('n',  '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-g>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('n', 's<left>', '<C-w>h')
+keymap.set('n', 's<up>', '<C-w>k')
+keymap.set('n', 's<down>', '<C-w>j')
+keymap.set('n', 's<right>', '<C-w>l')
+keymap.set('n', 'sh', '<C-w>h')
+keymap.set('n', 'sk', '<C-w>k')
+keymap.set('n', 'sj', '<C-w>j')
+keymap.set('n', 'sl', '<C-w>l')
+
+-- Resize windows
+keymap.set('n', '<C-w>h', '<C-w>4>')
+keymap.set('n', '<C-w>l', '<C-w>4<')
+keymap.set('n', '<C-w>k', '<C-w>2+')
+keymap.set('n', '<C-w>j', '<C-w>2-')
